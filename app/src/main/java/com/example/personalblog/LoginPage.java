d7e5da4a57996f6c1fe53fd5b65d6f4c966ee514
mprzywozny <23365021@studentmail.ul.ie>
2026-09-21 20:23:39 +0100

    Synced the file with firebase

--- app/src/main/java/com/example/personalblog/LoginPage.java
+++ app/src/main/java/com/example/personalblog/LoginPage.java
@@ -1,6 +1,8 @@
 package com.example.personalblog;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginPage extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    Button loginButton;
+    Button registerButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,19 @@ public class LoginPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        email = findViewById(R.id.emailInput);
+        password = findViewById(R.id.passwordInput);
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
+
+        loginButton.setOnClickListener(view -> {
+            String emailText = email.getText().toString();
+            String passwordText = password.getText().toString();
+
+
+        });
+
+
     }
 }
