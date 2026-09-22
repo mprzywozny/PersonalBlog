ee0888748b0b5560ca94eecf3305035e9d20b6f4
mprzywozny <23365021@studentmail.ul.ie>
2026-09-22 09:09:18 +0100

    Add settings page

--- app/src/main/java/com/example/personalblog/SettingsPage.java
+++ app/src/main/java/com/example/personalblog/SettingsPage.java
@@ -1,6 +1,8 @@
 package com.example.personalblog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class SettingsPage extends AppCompatActivity {
+
+    private Button changeEmailButton, changePasswordButton, backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        changeEmailButton = findViewById(R.id.changeEmailButton);
+        changePasswordButton = findViewById(R.id.changePasswordButton);
+        backButton = findViewById(R.id.backButton);
+
+        changeEmailButton.setOnClickListener(view -> {
+            startActivity(new Intent(this, ChangeEmailPage.class));
+        });
+
+        changePasswordButton.setOnClickListener(view -> {
+            startActivity(new Intent(this, ChangePasswordPage.class));
+        });
+
+        backButton.setOnClickListener(view -> {
+            startActivity(new Intent(this, Homepage.class));
         });
     }
 }
