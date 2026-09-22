d5ff90752a8ecde04613a6876541c2bd93a9ec00
mprzywozny <23365021@studentmail.ul.ie>
2026-09-22 08:59:31 +0100

    Make homescreen

--- app/src/main/java/com/example/personalblog/Homepage.java
+++ app/src/main/java/com/example/personalblog/Homepage.java
@@ -1,6 +1,8 @@
 package com.example.personalblog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Homepage extends AppCompatActivity {
+
+    private Button createPostButton, viewProfileButton, settingsButton, signOutButton;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_homepage);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        createPostButton = findViewById(R.id.createPostButton);
+        viewProfileButton = findViewById(R.id.viewProfileButton);
+        settingsButton = findViewById(R.id.settingsButton);
+        signOutButton = findViewById(R.id.signOutButton);
+
+        auth = FirebaseAuth.getInstance();
+
+        createPostButton.setOnClickListener(view -> {
+            startActivity(new Intent(this, CreatePostPage.class));
         });
+
+        viewProfileButton.setOnClickListener(view -> {
+            startActivity(new Intent(this, ProfilePage.class));
+        });
+
+        settingsButton.setOnClickListener(view -> {
+            startActivity(new Intent(this, SettingsPage.class));
+        });
+
+        signOutButton.setOnClickListener(view -> {
+            auth.signOut();
+            startActivity(new Intent(this, LoginPage.class));
+        });
+
     }
 }
