068ab9b3f1204eb7f372459bccf3a3c46a467b17
mprzywozny <23365021@studentmail.ul.ie>
2026-09-22 11:29:05 +0100

    Implemented post functionality - users can post things and theyre stored in the firebase realtime database

--- app/src/main/java/com/example/personalblog/CreatePostPage.java
+++ app/src/main/java/com/example/personalblog/CreatePostPage.java
@@ -1,6 +1,10 @@
 package com.example.personalblog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,56 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+
+import java.util.HashMap;
+
 public class CreatePostPage extends AppCompatActivity {
+
+    private EditText postInput;
+    private Button confirmButton, cancelButton;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_post_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        postInput = findViewById(R.id.postInput);
+        confirmButton = findViewById(R.id.confirmButton);
+        cancelButton = findViewById(R.id.cancelButton);
+
+        auth = FirebaseAuth.getInstance();
+
+        confirmButton.setOnClickListener(view -> {
+            String content = postInput.getText().toString().trim();
+            if(!content.isEmpty()) {
+                writePost(auth.getCurrentUser().getEmail(), content);
+                Toast.makeText(this, "Posted!", Toast.LENGTH_LONG).show();
+                postInput.setText("");
+            } else{
+                Toast.makeText(this, "Your post cannot be empty", Toast.LENGTH_LONG).show();
+            }
         });
+
+        cancelButton.setOnClickListener(view -> {
+            startActivity(new Intent(CreatePostPage.this, Homepage.class));
+        });
+
+
+    }
+
+    private void writePost(String email, String content){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("posts").child(content);
+        HashMap<String, Object> post = new HashMap<>();
+        post.put("email", email);
+        post.put("content", content);
+        post.put("time", ServerValue.TIMESTAMP);
+        reference.push().setValue(post);
     }
 }
