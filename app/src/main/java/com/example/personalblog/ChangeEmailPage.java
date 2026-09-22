0ceb59e1659c35be0b20e8c7ce2960be4a4cb689
mprzywozny <23365021@studentmail.ul.ie>
2026-09-22 09:33:28 +0100

    Implement change email and password

--- app/src/main/java/com/example/personalblog/ChangeEmailPage.java
+++ app/src/main/java/com/example/personalblog/ChangeEmailPage.java
@@ -1,6 +1,10 @@
 package com.example.personalblog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ChangeEmailPage extends AppCompatActivity {
+
+    private EditText currentEmailText;
+    private EditText newEmailText;
+    private Button confirmButton;
+    private Button cancelButton;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_change_email_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        auth = FirebaseAuth.getInstance();
+
+        currentEmailText = findViewById(R.id.currentEmailInput);
+        newEmailText = findViewById(R.id.newEmailInput);
+        confirmButton = findViewById(R.id.confirmButton);
+        cancelButton = findViewById(R.id.cancelButton);
+
+        confirmButton.setOnClickListener(view -> {
+            String currentEmail = currentEmailText.getText().toString().trim();
+            String newEmail = newEmailText.getText().toString().trim();
+
+            if (currentEmail.isEmpty() || newEmail.isEmpty()) {
+                Toast.makeText(this, "Please enter your current and new email", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            if (!currentEmail.equals(auth.getCurrentUser().getEmail())) {
+                Toast.makeText(this, "Current email is incorrect", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            auth.getCurrentUser().updateEmail(newEmail).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(ChangeEmailPage.this, "Email updated successfully.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(ChangeEmailPage.this, "Email update failed", Toast.LENGTH_LONG).show();
+                }
+            });
         });
+
+        cancelButton.setOnClickListener(view -> {
+            startActivity(new Intent(this, SettingsPage.class));
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/personalblog/ChangePasswordPage.java
+++ app/src/main/java/com/example/personalblog/ChangePasswordPage.java
@@ -1,6 +1,10 @@
 package com.example.personalblog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,65 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ChangePasswordPage extends AppCompatActivity {
+
+    private EditText currentPasswordText;
+    private EditText newPasswordText;
+    private EditText confirmPasswordText;
+    private Button confirmButton;
+    private Button cancelButton;
+    private FirebaseAuth auth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_change_password_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        currentPasswordText = findViewById(R.id.currentPasswordInput);
+        newPasswordText = findViewById(R.id.newPasswordInput);
+        confirmPasswordText = findViewById(R.id.confirmPasswordInput);
+        confirmButton = findViewById(R.id.confirmButton);
+        cancelButton = findViewById(R.id.cancelButton);
+
+        auth = FirebaseAuth.getInstance();
+
+        confirmButton.setOnClickListener(view -> {
+            String currentPassword = currentPasswordText.getText().toString().trim();
+            String newPassword = newPasswordText.getText().toString().trim();
+            String confirmPassword = confirmPasswordText.getText().toString().trim();
+
+            if (currentPassword.isEmpty() || newPassword.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(this, "Please enter your current and new password", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            if (!newPassword.equals(confirmPassword)) {
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            if (newPassword.length() < 6) {
+                Toast.makeText(this, "Your password is too short", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            auth.getCurrentUser().updatePassword(newPassword).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(ChangePasswordPage.this, "Password updated successfully.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(ChangePasswordPage.this, "Password update failed", Toast.LENGTH_LONG).show();
+                }
+            });
+
+        });
+
+        cancelButton.setOnClickListener(view -> {
+            startActivity(new Intent(this, SettingsPage.class));
         });
     }
 }
